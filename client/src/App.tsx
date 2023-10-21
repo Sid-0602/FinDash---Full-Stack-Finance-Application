db0ac7adc2e894cb8f29f7fcb7b66a2e00111e06
Siddhant J <sidbnjadhav@gmail.com>
2023-10-21 20:38:28 +0530

    React Routing and Navigation Push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { themeSettings } from "./theme";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline, Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(()=> createTheme(themeSettings), [])
-  return <div className='app'>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      Hi!!
-    </ThemeProvider>
+  return (
+  <div className='app'>
+    <BrowserRouter >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<div>Dashboard Page</div>} />
+            <Route path="/predictions" element={<div>Predictions Page</div>} />
+          </Routes>
+        </Box>
+      </ThemeProvider>
+    </BrowserRouter>
   </div>
+  )
 }
 
 export default App;
