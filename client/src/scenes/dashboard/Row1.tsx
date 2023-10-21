4bcf99fa6c248e5e317c2880e2be00423f1b2f8a
Siddhant J <sidbnjadhav@gmail.com>
2023-10-22 00:02:22 +0530

    Redux store and Api for key performance indicators

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,7 +1,11 @@
 import DashboardBox from '@/components/DashboardBox'
+import { useGetKpisQuery } from '@/state/api';
+
 type Props = {}
 
 const Row1 = (props: Props) => {
+
+  const { data } = useGetKpisQuery(); //this will give an api call. 
   return (
     <>
         <DashboardBox gridArea="a" />
