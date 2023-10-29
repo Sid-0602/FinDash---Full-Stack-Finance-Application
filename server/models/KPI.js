9d45abf2d928d6b84bc56eefabf8cc87e9ffc65f
Siddhant J <sidbnjadhav@gmail.com>
2023-10-29 21:41:19 +0530

    Server update push

---
+++ server/models/KPI.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import { loadType } from "mongoose-currency";
+
+const Schema = mongoose.Schema;
+loadType(mongoose);
+
+const KPISchema = new Schema(
+    {
+        totalProfit: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v)=> v / 100
+        },
+        totalRevenue: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v)=> v / 100
+        },
+        totalExpenses: {
+            type: mongoose.Types.Currency,
+            currency: "USD",
+            get: (v)=> v / 100
+        },
+        expensesByCategory: {
+            type: Map,
+            of :{
+                type: mongoose.Types.Currency,
+                currency: "USD",
+                get: (v) => v/100
+            }
+        },
+    }
+);
+
+const KPI = mongoose.model("KPI",KPISchema);
+
+export default KPI;
